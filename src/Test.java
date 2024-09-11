693a4eab934b4db1e14530267a8e219fb734e127
Le Henaff Tangi <tlehenaff@fixe-h113-02>
2024-09-11 10:23:35 +0200

    ajout d'un second cadre avec quatre boutons

--- src/Test.java
+++ src/Test.java
@@ -5,6 +5,8 @@ public class Test {
     public static void main(String[] args) {
         ViewSimpleGame vsg = new ViewSimpleGame();
         vsg.affiche();
+        ViewCommand vc = new ViewCommand();
+        vc.affiche();
         SimpleGame sg= new SimpleGame(42);
         sg.launch();
         
--- src/ViewCommand.java
+++ src/ViewCommand.java
@@ -4,5 +4,45 @@ import java.awt.*;
  * Classe affichant les boutons de commandes
  */
 public class ViewCommand {
-    
+    private JFrame jFrame;
+
+    public ViewCommand(){
+        jFrame=new JFrame();
+        jFrame.setTitle("Game");
+        jFrame.setSize(new Dimension(700, 700));
+        Dimension windowSize = jFrame.getSize();
+        GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+        Point centerPoint = ge.getCenterPoint();
+        int dx = centerPoint.x - windowSize.width / 2 ;
+        int dy = centerPoint.y - windowSize.height / 2 +350;
+        jFrame.setLocation(dx, dy);
+        JPanel jp1=new JPanel(new GridLayout(2,1));
+        JPanel jp2=new JPanel(new GridLayout(1,2));
+        JPanel jp3=new JPanel(new GridLayout(1,2));
+
+        JButton jb1=new JButton(new ImageIcon("res/img/icon_restart.png"));
+        JButton jb2=new JButton(new ImageIcon("res/img/icon_play.png"));
+        JButton jb3=new JButton(new ImageIcon("res/img/icon_step.png"));
+        JButton jb4=new JButton(new ImageIcon("res/img/icon_pause.png"));
+
+        jp2.add(jb1);
+        jp2.add(jb2);
+        jp2.add(jb3);
+        jp2.add(jb4);
+
+        jp2.setVisible(true);
+
+        
+
+        jp1.add(jp2);
+        jp1.add(jp3);
+        jFrame.add(jp1);
+    }
+
+    /**
+     * rend le cadre visible
+     */
+    public void affiche(){
+        jFrame.setVisible(true);
+    }
 }
